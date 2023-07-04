6403a205a05205304ea191f2c504172fa39e797b
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-07-04 14:17:53 +0300

    commnts

--- src/Componants/Comments/Comments.jsx
+++ src/Componants/Comments/Comments.jsx
@@ -6,7 +6,41 @@ function Comments() {
     <div className='Comments'>
       
       <div className="comment-container">
-         <h1> Commments section </h1>
+        {/* <h1> Commments section </h1> */}
+        <div className="Comments-BigBox">
+
+          <div className="singleComment">
+
+            <div className="commentImg"></div>
+            <span className='commentName' ></span>
+            <input type="text" placeholder='lorem8' />
+            
+            <div className="singleComment-Replay">
+              <div className="commentImg"></div>
+              <span className='commentName' ></span>
+              <input type="text" placeholder='lorem8'/>
+            </div> {/** end singleComment-Replay */}
+            
+          </div> {/** end singleComment */}
+
+          
+          <div className="singleComment">
+            <div className="commentImg"></div>
+            <span className='commentName' ></span>
+            <input type="text" placeholder='lorem8'/>
+          </div> {/** end singleComment */}
+
+
+        </div> {/** end Comments-BigBox */}
+
+        <div className="commentsBtm">
+
+          <input type="text"  placeholder='leave your comments here '   />
+
+
+        </div> {/** end commentsBtm */}
+
+        <button className='buttonDefault' > Send </button>
 
 
       </div> {/** end comment-container */}
