fe217a5c5bda157ac1580aaf9e3d565e4b7c361e
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-07-08 15:35:26 +0300

    comnts

--- src/Componants2/Comments2/Comments2.jsx
+++ src/Componants2/Comments2/Comments2.jsx
@@ -6,13 +6,49 @@ function Comments2() {
     <div className='Comments2' >
 
       <div className="container">
-        
-        
-           <h1>Whereas disregard and contempt for human rights have resulted</h1>
+      
+        <div className="Comments2-container d-flex flex-column p-2  ">  
+          
+
+
+               <div className="Comments2-top d-flex  flex-column p-2">
+                  <div className="Comments2-top-box">
+              
+                       <div className="left-Comments2-top-box">
+                          <div className="left-Comments2-top-box-img">
+                          </div>{/** end left-Comments2-top-box-img */}
+                       </div>  {/** end left-Comments2-top-box */}
+                      
+                       <div className="right-Comments2-top-box">
+                          <div className="left-Comments2-top-box-title">
+                          </div> {/** end left-Comments2-top-box-title */}
+                          <div className="left-Comments2-top-box-comnt">
+                          </div> {/** end left-Comments2-top-box-comnt */}
+                       </div> {/** end right-Comments2-top-box */}
+              
+                  </div> {/** end Comments2-top-box */}
+              </div> {/**end  Comments2-top */}
+          
+
+             <div className="Comments2-btm d-flex p-2">
+                <div className="Comments2-btm-box d-flex flex-column p-2 w-100  rounded ">
+                    <input type="text" placeholder='Comments'   name="comments"  className='border-light border-0 rounded p-4 '  />
+                    <button className='border-0 p-2 text-white my-4' >Send</button>
+                </div> {/** end Comments2-btm-box */}
+                                   
+                                
+              </div> {/** end Comments2-btm */}
+             
+          
+          
+          </div> {/** end Comments2-container */}
+                    
+
 
       </div> {/** end container */}
 
     </div>  /** end Comments2 */
+
   )
 }
 
