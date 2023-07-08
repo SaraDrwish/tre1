02e3268b5d1581f9fedffa09ec33816092eec1d1
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-07-08 16:37:48 +0300

    comnts

--- src/Componants2/Comments2/Comments2.jsx
+++ src/Componants2/Comments2/Comments2.jsx
@@ -11,29 +11,80 @@ function Comments2() {
           
 
 
-               <div className="Comments2-top d-flex  flex-column p-2">
-                  <div className="Comments2-top-box">
+               <div className="Comments2-top d-flex  flex-column p-4 m-2">
+                  <div className="Comments2-top-box d-flex  flex-column p-3 m-auto w-100 m-2">
               
-                       <div className="left-Comments2-top-box">
-                          <div className="left-Comments2-top-box-img">
-                          </div>{/** end left-Comments2-top-box-img */}
-                       </div>  {/** end left-Comments2-top-box */}
-                      
-                       <div className="right-Comments2-top-box">
-                          <div className="left-Comments2-top-box-title">
-                          </div> {/** end left-Comments2-top-box-title */}
-                          <div className="left-Comments2-top-box-comnt">
-                          </div> {/** end left-Comments2-top-box-comnt */}
+                      <div className="Comments2-top-box-container d-flex  p-1 ">
+                
+                            <div className="left-Comments2-top-box d-flex justify-content-center align-items-center ">
+                               <div className="left-Comments2-top-box-img"></div>{/** end left-Comments2-top-box-img */}
+                            </div>  {/** end left-Comments2-top-box */}
+                            
+                            <div className="right-Comments2-top-box d-flex flex-column mx-4 w-100">
+                                <div className="left-Comments2-top-box-title m-2">
+                                  <input placeholder='name Noran Ali' value="name : Noran Ali" type="text" className='border-light border-0 rounded p-1 w-100' />
+                                </div> {/** end left-Comments2-top-box-title */}
+                                <div className="left-Comments2-top-box-comnt m-2">
+                                  <input placeholder='single Comment' type="text" className='border-light border-0 rounded p-1 w-100 '  />
+                                </div> {/** end left-Comments2-top-box-comnt */}
                        </div> {/** end right-Comments2-top-box */}
+                     </div> {/** end Comments2-top-box-container */}
               
-                  </div> {/** end Comments2-top-box */}
+                    {/* <div className="col-sm m-1"> */}
+                       <div className="subComnt d-flex flex-column m-1 ">
+                               <div className="Comments2-top-box-container d-flex  p-1 ">
+                
+                                  <div className="left-Comments2-top-box d-flex justify-content-center align-items-center ">
+                                    <div className="left-Comments2-top-box-img"></div>{/** end left-Comments2-top-box-img */}
+                                  </div>  {/** end left-Comments2-top-box */}
+                                  
+                                  <div className="right-Comments2-top-box d-flex flex-column mx-4 w-100">
+                                      <div className="left-Comments2-top-box-title m-2">
+                                        <input placeholder='name Noran Ali' value="name : Noran Ali" type="text" className='border-light border-0 rounded p-1 w-100' />
+                                      </div> {/** end left-Comments2-top-box-title */}
+                                      <div className="left-Comments2-top-box-comnt m-2">
+                                        <input placeholder='single Comment' type="text" className='border-light border-0 rounded p-1 w-100 '  />
+                                      </div> {/** end left-Comments2-top-box-comnt */}
+                                    </div> {/** end right-Comments2-top-box */}
+                                </div> {/** end Comments2-top-box-container */}
+                          </div>
+                          {/** end sub commnt */}
+                
+            {/* </div> */}
+            {/** end col */}
+                
+     
+                      <div className="Comments2-top-box-container d-flex  p-1 ">
+                                    
+                                                <div className="left-Comments2-top-box d-flex justify-content-center align-items-center ">
+                                                  <div className="left-Comments2-top-box-img"></div>{/** end left-Comments2-top-box-img */}
+                                                </div>  {/** end left-Comments2-top-box */}
+                                                
+                                                <div className="right-Comments2-top-box d-flex flex-column mx-4 w-100">
+                                                    <div className="left-Comments2-top-box-title m-2">
+                                                      <input placeholder='name Noran Ali' value="name : Noran Ali" type="text" className='border-light border-0 rounded p-1 w-100' />
+                                                    </div> {/** end left-Comments2-top-box-title */}
+                                                    <div className="left-Comments2-top-box-comnt m-2">
+                                                      <input placeholder='single Comment' type="text" className='border-light border-0 rounded p-1 w-100 '  />
+                                                    </div> {/** end left-Comments2-top-box-comnt */}
+                                                </div> {/** end right-Comments2-top-box */}
+                                  
+                       </div> {/** end Comments2-top-box-container */}
+              
+
+
+
+                      </div> {/** end Comments2-top-box */}
+              
+            
+                       
               </div> {/**end  Comments2-top */}
           
 
              <div className="Comments2-btm d-flex p-2">
                 <div className="Comments2-btm-box d-flex flex-column p-2 w-100  rounded ">
-                    <input type="text" placeholder='Comments'   name="comments"  className='border-light border-0 rounded p-4 '  />
-                    <button className='border-0 p-2 text-white my-4' >Send</button>
+                    <input type="text" placeholder='Comments' name="comments" className='border-light border-0 rounded p-4 '  />
+                    <button className='border-0 p-2 text-white ' >Send</button>
                 </div> {/** end Comments2-btm-box */}
                                    
                                 
